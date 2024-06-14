9f45d49ff5d9cdb56ed4452b4362e7cb385f2d6b
Harshit Kumar <146705495+Ashborn10@users.noreply.github.com>
2024-06-14 16:57:31 +0530

    Calculate the percentage contribution of each pizza type to total revenue.

--- Query11.sql
+++ Query11.sql
@@ -1,1 +1,19 @@
+-- Calculate the percentage contribution of each pizza type to total revenue.
 
+SELECT 
+    pizza_types.category,
+    (SUM(pizzas.price * order_details.quantity) / (SELECT 
+            SUM(order_details.quantity * pizzas.price)
+        FROM
+            order_details
+                JOIN
+            pizzas ON order_details.pizza_id = pizzas.pizza_id)) * 100 AS percentage
+FROM
+    order_details
+        JOIN
+    pizzas ON order_details.pizza_id = pizzas.pizza_id
+        JOIN
+    pizza_types ON pizza_types.pizza_type_id = pizzas.pizza_type_id
+GROUP BY pizza_types.category
+ORDER BY percentage DESC
+;
